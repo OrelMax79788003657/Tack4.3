dd85b83cfa54a892e1796b64f5472d578a6d1be8
USER <USER@DESKTOP-FRBRUEK>
2021-11-27 21:36:59 +0300

    Добавьте файлы проекта.

---
+++ Tack4.3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tack4._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 0;
+            int B = 0;
+            int C = 0;
+
+            Console.Write("Введите число A: ");
+            A = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите число B: ");
+            B = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите число C: ");
+            C = Convert.ToInt32(Console.ReadLine());
+
+            int value = 0;
+
+            int x = 0;
+            int y = 0;
+
+            while (x <= A - C)
+            {
+                y = 0;
+                while (y <= B - C)
+                {
+                    y += C;
+                    value++;
+                }
+                x += C;
+            }
+            Console.Write(value.ToString() + " квадратов поместится");
+            Console.ReadKey();
+
+        }
+    }
+}
